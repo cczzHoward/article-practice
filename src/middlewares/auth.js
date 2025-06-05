9c58901a9544fb71a4eca467a5070de5fd8ea4f5
Howard.Cheng <howardgg509@gmail.com>
2025-06-05 15:29:29 +0800

    feat: implement admin and self-access middleware for authorization

---
+++ src/middlewares/auth.js
@@ -0,0 +1,15 @@
+const responseUtils = require('../utils/response');
+
+function isAdmin(req, res, next) {
+    if (req.user.role === 'admin') return next();
+}
+
+function isSelfOrAdmin(req, res, next) {
+    if (req.user.role === 'admin' || req.user._id.toString() === req.params.id) return next();
+    return responseUtils.forbidden(res, 'You do not have permission to access this resource');
+}
+
+module.exports = {
+    isSelfOrAdmin,
+    isAdmin,
+};
