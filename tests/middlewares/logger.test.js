86b4216b39ce5fa803356a4127c0d28d5985145f
cczzHoward <howardgg509@gmail.com>
2025-06-09 23:41:49 +0800

    test: add unit tests for logger middleware to verify request logging functionality

--- tests/middlewares/logger.test.js
+++ tests/middlewares/logger.test.js
@@ -1,5 +1,26 @@
-describe('test', () => {
-    it('should run a test', () => {
-        expect(true).toBe(true);
+const { logRequest } = require('../../src/middlewares/logger');
+const logger = require('../../src/utils/logger');
+
+describe('Logger Middleware', () => {
+    let mockReq, mockRes, next;
+
+    beforeEach(() => {
+        mockReq = {
+            method: 'GET',
+            originalUrl: '/test-endpoint',
+        };
+        mockRes = {};
+        next = jest.fn();
+    });
+
+    it('should log the request method and URL', () => {
+        const logSpy = jest.spyOn(logger, 'info');
+
+        logRequest(mockReq, mockRes, next);
+
+        expect(logSpy).toHaveBeenCalledWith('[GET] /test-endpoint');
+        expect(next).toHaveBeenCalled();
+
+        logSpy.mockRestore();
     });
 });
