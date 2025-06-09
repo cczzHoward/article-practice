a540c8f5e89caa5de62b644fe27b974a1653ac79
cczzHoward <howardgg509@gmail.com>
2025-06-10 00:29:43 +0800

    test: refactor logger mock implementation in setup.js for clarity

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,8 @@
-jest.spyOn(require('../src/utils/logger'), 'info').mockImplementation(() => {});
-jest.spyOn(require('../src/utils/logger'), 'warn').mockImplementation(() => {});
-jest.spyOn(require('../src/utils/logger'), 'error').mockImplementation(() => {});
+const logger = require('../src/utils/logger');
+
+jest.spyOn(logger, 'info').mockImplementation(() => {});
+jest.spyOn(logger, 'warn').mockImplementation(() => {});
+jest.spyOn(logger, 'error').mockImplementation(() => {});
 
 jest.mock('mongoose', () => {
     const actual = jest.requireActual('mongoose');
