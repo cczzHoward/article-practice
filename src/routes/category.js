ec272f3816ba228da39a7f25f01d2d7cfe9f0850
cczzHoward <howardgg509@gmail.com>
2025-07-10 12:53:09 +0800

    feat: add GET endpoint for listing categories

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/category');
 
+router.get('/list', CategoryController.getAll);
+
 module.exports = router;
