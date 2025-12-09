f8d7b85025bc8a9052bbc2e8bb14efa582352a5d
cczzHoward <howardgg509@gmail.com>
2025-12-09 20:21:41 +0800

    feat: implement retry logic for MongoDB connection

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 
 const connectionString = process.env.MONGODB_URI;
-const connection = mongoose.createConnection(connectionString);
+const connection = mongoose.createConnection();
+
+const connectWithRetry = () => {
+    console.log('Attempting MongoDB connection...');
+    connection.openUri(connectionString).catch((err) => {
+        logger.error('MongoDB connection unsuccessful, retry after 5 seconds.', err);
+        setTimeout(connectWithRetry, 5000);
+    });
+};
 
 if (process.env.NODE_ENV !== 'test') {
     connection.on('connected', () => {
@@ -12,6 +20,8 @@ if (process.env.NODE_ENV !== 'test') {
     connection.on('disconnected', () => {
         console.log('MongoDB disconnected');
     });
+
+    connectWithRetry();
 }
 
 connection.on('error', (err) => {
