5d9e5c17d61e297cc79b25e6d0fa5845165447d1
cczzHoward <howardgg509@gmail.com>
2025-12-26 17:02:27 +0800

    feat: add winston-daily-rotate-file for log rotation and update logger configuration

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,12 +1,22 @@
 const winston = require('winston');
+require('winston-daily-rotate-file'); // 引入輪替插件
 const path = require('path');
 
 const logDir = path.join(__dirname, './logs');
 const { format } = winston;
 const { combine, timestamp, printf } = format;
+
 const myFormat = printf(({ level, message, timestamp }) => {
     return `${timestamp} ${level}: ${message}`;
 });
+
+// 定義通用的銷毀與壓縮規則
+const commonRotateConfig = {
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '90d',
+};
 
 const logger = winston.createLogger({
     level: 'info',
@@ -18,8 +28,19 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
-        new winston.transports.File({ filename: path.join(logDir, 'combined.log') }),
+
+        new winston.transports.DailyRotateFile({
+            filename: path.join(logDir, 'error-%DATE%.log'),
+            auditFile: path.join(logDir, '.error-audit.json'),
+            level: 'error',
+            ...commonRotateConfig,
+        }),
+
+        new winston.transports.DailyRotateFile({
+            filename: path.join(logDir, 'combined-%DATE%.log'),
+            auditFile: path.join(logDir, '.combined-audit.json'),
+            ...commonRotateConfig,
+        }),
     ],
 });
 
